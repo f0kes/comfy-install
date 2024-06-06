688bfdec18d894da7c8c69d9e66f70685cb108a7
f0kes <f0kesppokes@gmail.com>
2024-06-06 20:51:35 +0300

    +

--- install_comfy.sh
+++ install_comfy.sh
@@ -160,8 +160,8 @@ function my_download() {
 main
 pip install gdown
 gdown ${GOOGLE_DRIVE_FOLDER} -O ${INPUT_PATH} --folder
-cd /workspace/comfy-install/
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-dpkg -i cloudflared-linux-amd64.deb
-python /workspace/comfyui-install/cloudflare.py
-python /workspace/ComfyUI/main.py
+#cd /workspace/comfy-install/
+#wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+#dpkg -i cloudflared-linux-amd64.deb
+#python /workspace/comfy-install/cloudflare.py
+python /workspace/ComfyUI/main.py --listen
